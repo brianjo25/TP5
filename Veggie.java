5631a8d6f1b07412a503e4fe84cd96a173cdc8a8
unknown <bejon.loekito@gmail.com>
2023-08-07 20:58:02 +0700

    Menambahkan perhitungan diskon sesuai ketentuan

--- Veggie.java
+++ Veggie.java
@@ -1,3 +1,31 @@
-public class Veggie {
+public class Veggie extends Product{
     // TODO: Implement this class.
+    
+    private boolean isOrganic;
+    public Veggie(String name, int price, int stock, boolean isOrganic){
+        super(name, price, stock);
+        this.isOrganic = isOrganic;
+    }
+
+    @Override
+    public double checkDiscount(int quantity) {
+        if (isOrganic == true){
+            if (quantity >= 5){
+                return this.getPrice() * 0.2;
+            }
+            else if (quantity >= 3){
+                return this.getPrice()*0.1;
+            }
+
+        else{
+            if (quantity >= 5){
+                return this.getPrice() * 0.25;
+            }
+            else if (quantity >= 3){
+                return this.getPrice() * 0.20;
+            }
+            }
+        }
+        return 0;
+    }
 }
