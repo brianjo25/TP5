a9a5c78501340702fc7a837b7d71770f6cd5775d
unknown <bejon.loekito@gmail.com>
2023-08-07 20:59:35 +0700

    Membuat konstruktor dan menambahkan membuat getter

--- Product.java
+++ Product.java
@@ -1,9 +1,21 @@
 abstract class Product {
     // TODO: Implement this class.
+    private String name;
+    private int price;
+    private int stock;
+
+    public Product(String name, int price, int stock){
+        this.name = name;
+        this.price = price;
+        this. stock = stock;
+    }
     
-    public int checkDiscount() {
+    public double checkDiscount(int quantity) {
         // TODO: Implement this method.
         return 0;
     }
 
+   public int getPrice(){
+    return price;
+   }
 }
