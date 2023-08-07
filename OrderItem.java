3717b37bd34579a15574e46746fa52a7c723c46f
unknown <bejon.loekito@gmail.com>
2023-08-08 00:30:38 +0700

    Membuat konstruktor, perhitungan harga akhir, dan menambah getter

--- OrderItem.java
+++ OrderItem.java
@@ -2,12 +2,23 @@ public class OrderItem {
     private Product product;
     private int quantity;
 
-    OrderItem() {
-        // TODO: Implement this method.
-    };
+    public OrderItem(Product product , int quantity) {
+        this.product = product;
+        this.quantity=quantity;
+    }
 
     public int getFinalPrice() {
-        // TODO: Implement this method.
-        return 0;
+    double originalPrice = this.product.getPrice();
+    double discountPercentage = this.product.checkDiscount(this.quantity) / 100.0;
+    double discountedPrice = originalPrice * (1 - discountPercentage);
+    int finalPrice = (int) (discountedPrice * this.quantity);
+    return finalPrice;
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+    public int getQuantity(){
+        return quantity;
     }
 }
