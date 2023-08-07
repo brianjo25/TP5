0f9df89b070839bd5876cc6ca7e7b681320e4068
unknown <bejon.loekito@gmail.com>
2023-08-07 23:21:54 +0700

    Menyelesaiaikan bagian cart

--- Cart.java
+++ Cart.java
@@ -2,18 +2,33 @@ import java.util.ArrayList;
 
 public class Cart {
     private Customer customer;
-    private ArrayList<OrderItem> orderList;
+    private ArrayList<OrderItem> orderList= new ArrayList<>();
 
-    Cart() {
+    Cart(Customer customer) {
         // TODO: Implement this method.
-    };
-
-    public int getTotalPrice() {
-        // TODO: Implement this method.
-        return 0;
+        this.customer = customer;
     }
+
+    public double getTotalPrice() {
+        // TODO: Implement this method.
+        double totalPrice = 0;
+        for (OrderItem item : orderList){
+            totalPrice += item.getFinalPrice();  
+        }
+
+        if(totalPrice >= 300000 && this.customer.isPremium()){
+            double diskon = totalPrice * 0.1;
+            totalPrice -= diskon;
+        }
+        return totalPrice;
+    } 
 
     public void addOrderItem(OrderItem orderItem) {
         // TODO: Implement this method.
-    };
+        this.orderList.add(orderItem);
+    }
+
+    public ArrayList<OrderItem> getOrderItemList(){
+        return orderList;
+    }
 }
