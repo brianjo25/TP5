3fd8356ea453342b0f0636330ad1d59dca1641f4
unknown <bejon.loekito@gmail.com>
2023-08-07 20:57:36 +0700

    Mengerjakan perhitungan discount sesuai dengan ketentuan yang diminta

--- Fruit.java
+++ Fruit.java
@@ -1,3 +1,33 @@
-public class Fruit {
-    // TODO: Implement this class.
+public class Fruit extends Product {
+    private boolean isLocal;
+    public Fruit(String name, int price, int stock, boolean isLocal){
+        super(name, price, stock);
+        this.isLocal = isLocal;
+    }
+
+    public boolean getLocal(){
+        return isLocal;
+    }
+
+    @Override
+    public double checkDiscount(int quantity) {
+        if (isLocal == true){
+            if (quantity >= 5){
+                return this.getPrice() * 0.3;
+            }
+            else if (quantity >= 3){
+                return this.getPrice()*0.2;
+            }
+
+        else{
+            if (quantity >= 5){
+                return this.getPrice() * 0.2;
+            }
+            else if (quantity >= 3){
+                return this.getPrice() * 0.15;
+            }
+            }
+        }
+        return 0;
+    }
 }
