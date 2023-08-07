60a546b4ced9eb5373cbd6d29da84e0b9776f28a
unknown <bejon.loekito@gmail.com>
2023-08-07 23:23:52 +0700

    Menambahkan private-private class, membuat konstruktor, dan membuat getter

--- Customer.java
+++ Customer.java
@@ -1,3 +1,21 @@
 public class Customer {
     // TODO: Implement this class.
+    private String name;
+    private boolean isPremium;
+    private Cart cart = new Cart(this);
+
+    public Customer(String name , boolean isPremium){
+    this.name = name;
+    this.isPremium = isPremium;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public boolean isPremium(){
+        return isPremium;
+    }
+    public Cart getCart(){
+        return cart;
+    }
 }
